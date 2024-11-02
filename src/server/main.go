81a6471b3bc534304447c2dbec6a73f1d23ffff1
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-11-02 21:50:01 +0700

    fix: PrintState

--- src/server/main.go
+++ src/server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "src/server/api"
+// import "src/server/api"
+
+import (
+	// "src/server/internal/models"
+	"fmt"
+	"src/server/internal/services"
+)
 
 func main() {
-	api.Init()
+	// api.Init()
+	initial, final, _, _ := services.HillClimbingSideways(100)
+	fmt.Printf("Value: %d\n", initial.Value)
+	initial.PrintState()
+	fmt.Println("============================")
+	fmt.Printf("Value: %d\n", final.Value)
+	final.PrintState()
 }
