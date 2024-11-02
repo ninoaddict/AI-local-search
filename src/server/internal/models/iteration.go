1925e5cfc0b196a33e6b8db605054db837fd5242
ninoaddict <adrilbless37@gmail.com>
2024-11-03 02:56:41 +0700

    feat: genetic algo

--- src/server/internal/models/iteration.go
+++ src/server/internal/models/iteration.go
@@ -9,6 +9,11 @@ type Iteration struct {
 	Exp    float64
 }
 
+type GeneticIteration struct {
+	AvgValue float64
+	MinValue int
+}
+
 func (iter Iteration) String() string {
 	return fmt.Sprintf("Iteration { First: %d, Second: %d, Value: %d, Exp: %.2f }", iter.First, iter.Second, iter.Value, iter.Exp)
 }
--- src/server/main.go
+++ src/server/main.go
@@ -3,21 +3,28 @@ package main
 // import "src/server/api"
 
 import (
-	// "src/server/internal/models"
 	"fmt"
+	"math/rand"
 	"src/server/internal/services"
+	"time"
 )
 
 func main() {
-	// api.Init()
-	initial, final, iterations, _, _ := services.SimulatedAnnealing()
-	fmt.Printf("Value: %d\n", initial.Value)
-	initial.PrintState()
-	fmt.Println("============================")
-	fmt.Printf("Value: %d\n", final.Value)
-	final.PrintState()
-
-	for index, iter := range iterations {
-		fmt.Printf("iter: %d, %v\n", index, iter)
+	rand.Seed(time.Now().UnixNano())
+	init, fin, _, _, _ := services.GeneticAlgorithm(10, 4)
+	for i := 0; i < len(init); i++ {
+		fmt.Printf("Value: %d\n", init[i].Value)
+		init[i].PrintState()
+		fmt.Println("============================")
 	}
+	fmt.Println("============================")
+
+	for i := 0; i < len(fin); i++ {
+		fmt.Printf("Value: %d\n", fin[i].Value)
+		fin[i].PrintState()
+		fmt.Println("============================")
+	}
+	// for index, iter := range iterations {
+	// 	fmt.Printf("iter: %d, %d, %f\n", index, iter.MinValue, iter.AvgValue)
+	// }
 }
