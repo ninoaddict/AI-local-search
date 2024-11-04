46b50dbcc9edd9e06c28e721679775a8c7a8a537
Berto Richardo Togatorop <bertotogatorop27@gmail.com>
2024-11-04 18:46:54 +0700

    fix: objective value display for random restart

--- src/clients/src/App.tsx
+++ src/clients/src/App.tsx
@@ -104,7 +104,7 @@ const App = () => {
                   <div>
                     <ValuePlot
                       chartData={iteration.Iter}
-                      description={"Iteration: " + index + 1}
+                      description={"Iteration: " + (index + 1)}
                     />
                   </div>
                 ))}
@@ -113,7 +113,7 @@ const App = () => {
                 {responseData.iterations.map((iteration, index) => (
                   <div>
                     <p>
-                      Iteration for restart {index} : {iteration.NumIter}
+                      Iteration for restart {index + 1} : {iteration.NumIter}
                     </p>
                     <VideoPlayer
                       key={index}
@@ -150,9 +150,7 @@ const App = () => {
                 />
 
                 <div className="mt-6 text-xl font-bold text-center">PLOT</div>
-                <ValuePlot
-                  chartData={responseData.iterations}
-                />
+                <ValuePlot chartData={responseData.iterations} />
 
                 <div className="mt-6 text-xl font-bold text-center">VIDEO</div>
                 <VideoPlayer
